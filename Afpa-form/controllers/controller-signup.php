6e2e9ec62458a911dc562606a8c086ce232893e4
ZakeFlorian <83126049+ZakeFlorian@users.noreply.github.com>
2024-01-14 21:19:44 +0100

    Plus que du CSS

--- Afpa-form/controllers/controller-signup.php
+++ Afpa-form/controllers/controller-signup.php
@@ -1,3 +1,56 @@
 <?php
-include ('./views/view-signup.php');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $birthdate = $_POST['birthdate'];
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    if (empty($name)) {
+        echo "Champ 'Nom' obligatoire <br>";
+    } else {
+        echo "Nom : " . $name . "<br>";
+    }
+    if (empty($prenom)) {
+        echo "Champ 'Prénom' obligatoire <br>";
+    } else {
+        echo "Prénom : " . $prenom . "<br>";
+    }
+    if (empty($birthdate)) {
+        echo "Champ 'Date de naissance' obligatoire <br>";
+    } else {
+        echo "Date de naissance : " . $birthdate . "<br>";
+    }
+    if (empty($mail)) {
+        echo "Champ 'Adresse mail' obligatoire <br>";
+    } else {
+        echo "Adresse mail : " . $mail . "<br>";
+    }
+    if (empty($password)) {
+        echo "Champ 'Mot de passe' obligatoire <br>";
+    } else {
+        echo "Mot de passe : Merci <br>";
+    }
+    if (empty($confirmPassword) || $confirmPassword !== $password) {
+        echo "Les mots de passe ne correspondent pas <br>";
+    } else {
+        echo "Les mots de passes sont identiques <br>";
+    }
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include('../views/view-signup.php');
+?>
+
+</body>
+</html>
--- Afpa-form/views/view-signup.php
+++ Afpa-form/views/view-signup.php
@@ -1,38 +1,62 @@
-<?php
-include ('./views/templates/header.php')
-include ('./views/templates/footer.php')
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Formulaire d'inscription</title>
 </head>
 <body>
-<form>
-  <div class="mb-3">
-    <label for="nomInput" class="form-label">Nom</label>
-    <input type="text" class="form-control" id="nomInput">
-  </div>
-  <div class="mb-3">
-    <label for="prénomInput" class="form-label">Prénom</label>
-    <input type="text" class="form-control" id="prénomInput">
-  </div>
-  <div class="datePicker">
-  <template>
-  <div>
-    <b-form-datepicker v-model="value" :date-disabled-fn="dateDisabled" locale="en"></b-form-datepicker>
-  </div>
-</template>
-</div>
-  <!-- <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="robotCheck">
-    <label class="form-check-label" for="robotCheck">Je ne suis pas un robot</label>
-  </div> -->
+<?php
+include ('../views/templates/header.php');
+?>
+<div class="d-flex flex-column container">
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
+            <label for="nom">Nom :
+                <input type="text" id="nom" name="nom" size="50" placeholder="ex:DOE"
+                    value="<?php if (!empty($name)) {
+                        echo $name;
+                    } ?>" required>
+            </label>
+            <label for="prenom" >Prénom :
+                <input type="text" id="prenom" name="prenom" size="50" placeholder="ex:Peter"
+                    value="<?php if (!empty($prenom)) {
+                        echo $prenom;
+                    } ?>">
+            </label>
+            <label for="birthdate" >Date de naissance :
+                <input type="date" id="birthdate" name="birthdate" size="10"
+                    value="<?php if (!empty($birthdate)) {
+                        echo $birthdate;
+                    } ?>">
+            </label>
+            <label for="mail" >Courriel :
+                <input type="text" id="mail" name="mail" size="50" placeholder="Ex:mon-mail@mail.fr"
+                    value="<?php if (!empty($mail)) {
+                        echo $mail;
+                    } ?>">
+            </label>
+            <label for="password">Mot de passe :
+                <input type="password" id="password" name="password" size="50"
+                    value="<?php if (!empty($password)) {
+                        echo 'correct';
+                    } ?>">
+            </label>
+            <label for="confirmPassword" >Confirmation du mot de passe :
+                <input type="password" id="confirmPassword" name="confirmPassword" size="20"
+                    value="<?php if (!empty($confirmPassword)) {
+                        echo 'correct';
+                    } ?>">
+            </label>
+            <input class="btn-signup" type="submit" value="S'enregistrer">
+        </form>
+                  </div>
 
-  <button type="submit" class="btn btn-primary">S'enregistrer'</button>
-</form>
+<?php
+include ('../views/templates/footer.php');
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
