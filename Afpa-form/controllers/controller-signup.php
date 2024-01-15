6d34def41114ba63ea4833f9f23b8956e0668fce
ZakeFlorian <83126049+ZakeFlorian@users.noreply.github.com>
2024-01-15 16:34:20 +0100

    Link BDD + Form done

--- Afpa-form/controllers/controller-signup.php
+++ Afpa-form/controllers/controller-signup.php
@@ -1,68 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-include('../views/view-signup.php');
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $birthdate = $_POST['birthdate'];
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirmPassword'];
+    <?php
+    require("../config.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $errors = [];
 
-    function afficherMessageErreur($champ, $message) {
-        echo "<p style='color: red;'>$message</p>";
-        echo "<script>document.getElementById('$champ').style.border = '1px solid red';</script>";
+
+        if (empty($_POST['nom'])) {
+            $errors['nom'] = "Veuillez saisir un nom";
+        } 
+
+        if (empty($_POST['prenom'])) {
+            $errors['prenom'] = "Veuillez saisir un prenom";
+        }
+
+        if (empty($_POST['pseudo'])) {
+            $errors['pseudo'] = "Veuillez saisir un pseudo";
+        } 
+
+        $birthdate = $_POST['birthdate'];
+// Contrôle de la date de naissance
+    if (empty($_POST['birthdate'])) {
+        $errors["spanBirthdate"] = "Le champ Date de naissance ne peut pas être vide";
     }
 
-    function afficherValeur($label, $valeur) {
-        echo "$label : $valeur <br>";
-    }
+        if (empty($_POST['mail'])) {
+            $errors['mail'] = "Veuillez saisir un mail";
+        } 
 
-    if (empty($name)) {
-        afficherMessageErreur('nom', "Champ 'Nom' obligatoire");
-    } else {
-        afficherValeur("Nom", $name);
-    }
+        if (!isset($_POST["enterprise"])) {
+            $errors["enterprise"] = "Veuillez selectionner une entreprise";
+        } 
 
-    if (empty($prenom)) {
-        afficherMessageErreur('prenom', "Champ 'Prénom' obligatoire");
-    } else {
-        afficherValeur("Prénom", $prenom);
-    }
+        if (empty($_POST['password'])) {
+            $errors['password'] = "Veuillez saisir un mot de passe";
+        }
 
-    if (empty($birthdate)) {
-        afficherMessageErreur('birthdate', "Champ 'Date de naissance' obligatoire");
-    } else {
-        afficherValeur("Date de naissance", $birthdate);
-    }
+        if (empty($_POST['confirmPassword'])) {
+            $errors['confirmPassword'] = "Veuillez saisir un mot de passe";
+        } else if (!empty($_POST['password'])){
+            if($_POST['confirmPassword'] !== $_POST['password']) {
+            $errors['confirmPassword'] = "Les mots de passes ne correspondent pas";
+        } 
+        }
 
-    if (empty($mail)) {
-        afficherMessageErreur('mail', "Champ 'Adresse mail' obligatoire");
-    } else {
-        afficherValeur("Adresse mail", $mail);
-    }
+        if (!isset($_POST["CGU"])) {
+            $errors["CGU"] = "Veuillez accepter nos CGU";
+        }
 
-    if (empty($password)) {
-        afficherMessageErreur('password', "Champ 'Mot de passe' obligatoire");
-    } else {
-        afficherValeur("Mot de passe", "Merci");
-    }
+        if (empty($errors)) {
 
-    if (empty($confirmPassword) || $confirmPassword !== $password) {
-        afficherMessageErreur('confirmPassword', "Les mots de passe ne correspondent pas");
-    } else {
-        afficherValeur("Les mots de passes", "sont identiques");
+            try {
+
+                $dbName = 'ecoride';
+                $dbUser = 'ecoride';
+                $dbPassword = 'ecoride';
+
+                $db = new PDO("mysql:host=localhost;dbname=" . DBNAME, DBUSER, DBPASS);
+                var_dump($db);
+
+                $sql = "INSERT INTO `utilisateur` (`firstname_utilisateur`, `lastname_utilisateur`, `nickname_utilisateur`,`birthdate_utilisateur`,`email_utilisateur`,`password_utilisateur`,`ID_Entreprise`) VALUES ( :firstname, :lastname, :pseudo, :birthdate, :email, :password_utilisateur, :id_enterprise) ";
+
+                $query = $db->prepare($sql);
+
+                $nom = htmlspecialchars($_POST['nom']);
+                $prenom = $_POST['prenom'];
+                $pseudo = htmlspecialchars($_POST["pseudo"]);
+                $birthdate = $_POST['birthdate'];
+                $email = $_POST['mail'];
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $enterprise = $_POST['enterprise'];
+
+                //on relie les valeurs à nos marqueurs nominatifs
+                $query->bindValue(':lastname', $nom, PDO::PARAM_STR);
+                $query->bindValue(':firstname', $prenom, PDO::PARAM_STR);
+                $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+                $query->bindValue(':birthdate', $birthdate, PDO::PARAM_STR);
+                $query->bindValue(':email', $email, PDO::PARAM_STR);
+                $query->bindValue(':password_utilisateur', $password, PDO::PARAM_STR);
+                $query->bindValue(':id_enterprise', $enterprise, PDO::PARAM_STR);
+
+                //on execute la requête
+                $query->execute();
+            } catch (PDOException $e) {
+                echo 'Erreur :' . $e->getMessage();
+            }
+        }
     }
-}
-?>
+    include('../views/view-signup.php');
+    ?>
 
 
 </body>
+
 </html>
--- Afpa-form/views/view-signup.php
+++ Afpa-form/views/view-signup.php
@@ -1,11 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Formulaire d'inscription</title>
     <style>
         .container {
@@ -16,55 +17,104 @@
         }
     </style>
 </head>
+
 <body class="bg-black">
-<?php
-include ('../views/templates/header.php');
-?>
-<div class="container mt-5">
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
-        <div class="mb-3">
-            <label for="nom" class="form-label text-white">Nom :</label>
-            <input type="text" id="nom" name="nom" class="form-control" size="50" placeholder="ex:DOE"
-                value="<?php if (!empty($name)) { echo $name; } ?>" required>
-        </div>
+    <?php
+    include('../views/templates/header.php');
+    ?>
+    <div class="container mt-5">
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
+            <div class="mb-3">
+                <label for="nom" class="form-label text-white">Nom :</label>
+                <input type="text" id="nom" name="nom" class="form-control" size="50" placeholder="ex:DOE"
+                    value="<?= $_POST['nom'] ?? '' ?>" required>
+                <span class="input-warning text-danger">
+                    <?= $errors['nom']?? '' ?>
+                </span>
+            </div>
 
-        <div class="mb-3">
-            <label for="prenom" class="form-label text-white">Prénom :</label>
-            <input type="text" id="prenom" name="prenom" class="form-control" size="50" placeholder="ex:Peter"
-                value="<?php if (!empty($prenom)) { echo $prenom; } ?>">
-        </div>
+            <div class="mb-3">
+                <label for="prenom" class="form-label text-white">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" class="form-control" size="50" placeholder="ex:Peter"
+                    value="<?= $_POST['prenom'] ?? '' ?>">
+                <span class="input-warning text-danger">
+                    <?= $errors['prenom']?? '' ?>
+                </span>
+            </div>
 
-        <div class="mb-3">
-            <label for="birthdate" class="form-label text-white">Date de naissance :</label>
-            <input type="date" id="birthdate" name="birthdate" class="form-control" size="10"
-                value="<?php if (!empty($birthdate)) { echo $birthdate; } ?>">
-        </div>
+            <div class="mb-3">
+                <label for="pseudo" class="form-label text-white">Pseudo :</label>
+                <input type="text" id="pseudo" name="pseudo" class="form-control" size="50" placeholder="ex:Zake"
+                    value="<?= $_POST['pseudo'] ?? '' ?>">
+                <span class="input-warning text-danger">
+                    <?= $errors['pseudo']?? '' ?>
+                </span>
+            </div>
 
-        <div class="mb-3">
-            <label for="mail" class="form-label text-white">Courriel :</label>
-            <input type="text" id="mail" name="mail" class="form-control" size="50" placeholder="Ex:mon-mail@mail.fr"
-                value="<?php if (!empty($mail)) { echo $mail; } ?>">
-        </div>
+            <label for="birthdate" class="text-white">Date de naissance<sup class="star">*</sup> :
+                <input class="inputField" type="date" id="birthdate" name="birthdate" value="<?php if (!empty($birthdate)) {
+                    echo $birthdate;
+                } ?>" required>
+                <span class="redInput spanBirthdate">
+                <?= isset( $errors["spanBirthdate"])? $errors["spanBirthdate"]:""?>
+                </span>
+            </label>
 
-        <div class="mb-3">
-            <label for="password" class="form-label text-white">Mot de passe :</label>
-            <input type="password" id="password" name="password" class="form-control" size="50"
-                value="<?php if (!empty($password)) { echo 'correct'; } ?>">
-        </div>
+            <div class="mb-3">
+                <label for="mail" class="form-label text-white">Courriel :</label>
+                <input type="text" id="mail" name="mail" class="form-control" size="50"
+                    placeholder="Ex:mon-mail@mail.fr" value="<?= $_POST['mail'] ?? '' ?>">
+                <span class="input-warning text-danger">
+                    <?= $errors['mail']?? '' ?>
+                </span>
+            </div>
 
-        <div class="mb-3">
-            <label for="confirmPassword" class="form-label text-white">Confirmation du mot de passe :</label>
-            <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" size="20"
-                value="<?php if (!empty($confirmPassword)) { echo 'correct'; } ?>">
-        </div>
+            <div class="mb-3">
+                <select name="enterprise" class="rounded" id="enterprise">
+                    <option value="">--Veuillez selectionner une entreprise--</option>
+                    <option value="1" <?= isset($_POST['enterprise'])&&$_POST['enterprise']==1 ? 'selected' : ''?>>LunaTech Solutions</option>
+                    <option value="2" <?= isset($_POST['enterprise'])&&$_POST['enterprise']==2 ? 'selected' : ''?>>Phoenix Consulting and Design</option>
+                </select>
+                <span class="input-warning text-danger">
+                    <?= $errors['enterprise']?? '' ?>
+                </span>
+            </div>
 
-        <input class="btn btn-primary" type="submit" value="S'enregistrer">
-    </form>
-</div>
+            <div class="mb-3">
+                <label for="password" class="form-label text-white">Mot de passe :</label>
+                <input type="password" id="password" name="password" class="form-control" size="50" value="<?= $_POST['password'] ?? '' ?>">
+                <span class="input-warning text-danger">
+                    <?= $errors['password']?? '' ?>
+                </span>
+            </div>
 
-<?php
-include ('../views/templates/footer.php');
-?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            <div class="mb-3">
+                <label for="confirmPassword" class="form-label text-white">Confirmation du mot de passe :</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" size="20" value="<?= $_POST['confirmPassword'] ?? '' ?>">
+                <span class="input-warning text-danger">
+                    <?= $errors['confirmPassword']?? '' ?>
+                </span>
+            </div>
+
+            <div class="mb-3">
+                <label for="CGU" class="text-white">Conditions générales d'utilisation</label>
+                <input type="checkbox" id="CGU" name="CGU" <?= isset($_POST['CGU'])?'checked' : '' ?>>
+                <span class="input-warning text-danger">
+                    <?= $errors['CGU']??'' ?>
+                </span>
+            </div>
+            
+
+            <input class="btn btn-primary" type="submit" value="S'enregistrer">
+        </form>
+    </div>
+
+    <?php
+    include('../views/templates/footer.php');
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
