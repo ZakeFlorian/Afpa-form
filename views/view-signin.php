db408353a2991cf82c8b3fb1d1cbab75214cb307
Florian <stephe76bis4@outlook.fr>
2024-05-21 21:54:24 +0200

    bdd

--- views/view-signin.php
+++ views/view-signin.php
@@ -8,6 +8,7 @@ require_once "../controllers/controller-signin.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="../../assets/css/style.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
     <title>Form Signin</title>
     <style>
         .container {
